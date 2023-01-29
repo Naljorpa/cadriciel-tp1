1c344093c94e7253d719cb03d0305bae7fe92678
Nicolas Fontaine <nikoniko.fontaine@gmail.com>
2023-01-28 23:49:36 -0500

    modification du style de la page de modification

--- resources/views/etudiant/edit.blade.php
+++ resources/views/etudiant/edit.blade.php
@@ -1,59 +1,86 @@
 @extends('layouts.app')
 @section('title','Modifier')
 @section('content')
+
 <div class="container">
     <div class="row">
-        <div class="col-12 text-center mt-2">
-            <h1 class="display-one">Modifier un étudiant</h1>
+        <div class="col-12 text-center pt-5">
+            <a href="/etudiant/{{$etudiant->id}}" class="btn btn-outline-maisonneuve btn-sm mb-5">Retourner</a>
+            <div class="blue-maisonneuve pt-3 pb-5">
+                <h1 class="display-one mt-5 blue-maisonneuve-h1">Modifier un étudiant</h1>
+            </div>
         </div>
     </div>
 </div>
-<hr>
-<div class="row justify-content-center">
-    <div class="col-6">
-        <div class="card">
-            <form method="post">
-                @csrf
-                @method('put')
-                <div class="card-header">
-                    Formulaire
-                </div>
-                <div class="card-body">
-                    <div class="control-group col-12">
-                        <label for="nom">Nom</label>
-                        <input type="text" id="title" name="nom" class="form-control"  value="{{$etudiant->nom}}">
-                    </div>
-                    <div class="control-group col-12">
-                        <label for="addresse">Addresse</label>
-                        <textarea name="addresse" id="body" class="form-control">{{$etudiant->addresse}}</textarea>
-                    </div>
-                    <div class="control-group col-12">
-                        <label for="phone">Téléphone</label>
-                        <input type="text" name="phone" id="body" class="form-control" value="{{$etudiant->phone}}"></input>
-                    </div>
-                    <div class="control-group col-12">
-                        <label for="email">Courriel</label>
-                        <input type="email" name="email" id="body" class="form-control" value="{{$etudiant->email}}"></input>
-                    </div>
-                    <div class="control-group col-12">
-                        <label for="date_de_naissance">Date de naissance</label>
-                        <input type="date" name="date_de_naissance" id="body" class="form-control" value="{{$etudiant->date_de_naissance}}"></input>
-                    </div>
-                    <div class="control-group col-12">
-                        <label for='ville_id'>Choisissez votre ville: </label>
-                        <select name='ville_id' id='villes-select'>
-                            <option value='{{$etudiant->etudiantHasVille->id}}'>{{$etudiant->etudiantHasVille->nom}}</option>
-                            @foreach($villes as $ville)
-                            <option value="{{$ville->id}}">{{$ville->nom}}</option>
-                            @endforeach
-                        </select>
 
-                    </div>
+<!-- inspiration pour le form: https://bbbootstrap.com/snippets/simple-contact-form-74408136 -->
+
+    <div class="row ">
+    <div class="col-lg-7 mx-auto mb-5">
+        <div class="card mt-5 mx-auto p-4 bg-light">
+            <div class="card-body bg-light">
+                <div class="container">
+                    <form method="post">
+                        @csrf
+                        @method('put')
+                        <div class="controls">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="nom">Nom *</label>
+                                        <input type="text" name="nom" class="form-control" placeholder="Svp entrez votre nom" required="required" value="{{$etudiant->nom}}">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="email">Courriel *</label>
+                                        <input type="email" name="email" class="form-control" value="{{$etudiant->email}}" required="required">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="addresse">Adresse *</label>
+                                        <input type="text" name="addresse" class="form-control" value="{{$etudiant->addresse}}" required="required">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for='ville_id'>Choisissez votre ville: </label>
+                                        <select name="ville_id" class="form-control" required="required">
+                                            <option value='{{$etudiant->etudiantHasVille->id}}'>{{$etudiant->etudiantHasVille->nom}}</option>
+                                            @foreach($villes as $ville)
+                                            <option value="{{$ville->id}}">{{$ville->nom}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="phone">Téléphone</label>
+                                        <input type="text" name="phone" id="body" class="form-control" value="{{$etudiant->phone}}" required="required"></input>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="date_de_naissance">Date de naissance</label>
+                                        <input type="date" name="date_de_naissance" id="body" class="form-control" required="required" value="{{$etudiant->date_de_naissance}}"></input>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <input type="submit" value="sauvegarder" class="btn btn-success pt-2 btn-block">
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div class="card-footer">
-                    <input type="submit" value="sauvegarder" class="btn btn-success">
-                </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
